7f033902e7230de3ef01ca4a8377bda87a949980
Mark Robertshaw <mark@MacBook-Pro.local>
2022-01-20 16:28:24 +0000

    New Kinimailer project

---
+++ php/test/TestBase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kinimailer;
+
+use Kiniauth\Bootstrap;
+use Kinikit\Core\DependencyInjection\Container;
+use Kinikit\Persistence\Tools\TestDataInstaller;
+
+class TestBase extends \PHPUnit\Framework\TestCase {
+
+
+    private static $run = false;
+
+    public static function setUpBeforeClass(): void {
+
+        $bootstrap = Container::instance()->get(Bootstrap::class);
+        $bootstrap->setup();
+
+        if (!self::$run) {
+
+            $testDataInstaller = Container::instance()->get(TestDataInstaller::class);
+            $testDataInstaller->run(true, ["../src"]);
+            self::$run = true;
+
+        }
+    }
+
+
+}
---
+++ php/test/autoloader.php
@@ -0,0 +1,24 @@
+<?php
+
+// Call the core autoloader.
+include_once __DIR__ . "/../../vendor/autoload.php";
+
+/**
+ * Test autoloader - includes src one as well.
+ */
+spl_autoload_register(function ($class) {
+
+    // Now check for source classes.
+    $srcClass = str_replace("Kinimailer\\", "", $class);
+    if ($srcClass !== $class) {
+        $file = DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $srcClass) . '.php';
+        if (file_exists(__DIR__ . $file)) {
+            require __DIR__ . $file;
+            return true;
+        }
+    }
+
+
+    return false;
+
+});
